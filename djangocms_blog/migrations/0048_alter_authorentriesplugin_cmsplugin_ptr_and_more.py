c7974a8523f3c9463d2dffc5ff3849013ca0bf2a
mihalikv <mihalikv@users.noreply.github.com>
2023-06-13 13:57:30 +0200

    triv: added django 4.2 migrations

---
+++ djangocms_blog/migrations/0048_alter_authorentriesplugin_cmsplugin_ptr_and_more.py
@@ -0,0 +1,38 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cms', '0022_auto_20180620_1551'),
+        ('djangocms_blog', '0047_post_main_mobile_image'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='authorentriesplugin',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='%(app_label)s_%(class)s', serialize=False, to='cms.cmsplugin'),
+        ),
+        migrations.AlterField(
+            model_name='genericblogplugin',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='%(app_label)s_%(class)s', serialize=False, to='cms.cmsplugin'),
+        ),
+        migrations.AlterField(
+            model_name='latestpostsplugin',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='%(app_label)s_%(class)s', serialize=False, to='cms.cmsplugin'),
+        ),
+        migrations.AlterField(
+            model_name='mostreadplugin',
+            name='cmsplugin_ptr',
+            field=models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, related_name='%(app_label)s_%(class)s', serialize=False, to='cms.cmsplugin'),
+        ),
+        migrations.AlterField(
+            model_name='mostreadplugin',
+            name='template_folder',
+            field=models.CharField(choices=[('plugins', 'Default template'), ('blog_list', 'Blog list')], default='plugins', help_text='Select plugin template to load for this instance', max_length=200, verbose_name='Plugin template'),
+        ),
+    ]
